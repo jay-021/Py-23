020f4fe504644b17b52de756d6b4e1ac4a04c15f
jay-021 <jaymathukiya021@gmail.com>
2023-09-24 17:36:57 +0300

    Fix problems and normal program done

--- rinku_q1.py
+++ rinku_q1.py
@@ -1,56 +1,62 @@
-def main ():
-    name= input("Welcome to my clothing shop , please enter your name: ")
-    print("What do you want to buy ? ")
-    item = input("shirt,pent,bra, top & one piece : ").lower()
-    print("Which color do you want ?")
-    color = input("white, black , pink , blue & Gray : ").lower()
+def main():
+    name = input("Welcome to my clothing shop, please enter your name: ")
+
+    print("What do you want to buy?")
+    item_select = input("shirt, pant, bra, top & one piece: ").lower()
+
+    print("Which color do you want?")
+    color_select = input("white, black, pink, blue & gray: ").lower()
+
+    item_price = item(item_select)
+    color_price = color(color_select)
+
+    total_price = item_price + color_price
+    print("Your total price without any discount is:", total_price)
+
     discount = int(input("How much discount do you want? "))
-    
+    discounted_price = total_price - (total_price * (discount / 100))  # Corrected the discount calculation
+    print("Hello", name, "Your final price after a", discount, "% discount is:", discounted_price)
+
 def item(i):
-    # while i==True:
     match i:
         case "shirt":
-            item1 = "shirt"
+            item1 = 500
             print("RS. 500")
-    match i:
-        case "pent":
-            item1 = "pent"
+        case "pant":  # Corrected the spelling of 'pant'
+            item1 = 550
             print("RS. 550")
-    match i:
         case "top":
-            item1 = "top"
+            item1 = 500
             print("RS. 500")
-    match i:
         case "bra":
-            item1 = "bra"
+            item1 = 450
             print("RS. 450")
-    match i:
         case "one piece":
-            item1 = "one piece"
-            print("RS. 1000")  
-    return item1            
+            item1 = 1000
+            print("RS. 1000")
+
+    return item1
 
 def color(c):
-    def item(c):
-    # while i==True:
     match c:
         case "black":
-            item2 = "black"
-            print("NOrmal")
-    match c:
+            item2 = 0
+            print("Normal")
         case "white":
-            item2 = "white"
-            print("NOrmal")
-    match c:
-        case "Gray":
-            item2 = "gray"
-            print("extra 100 rs")
-    match c:
+            item2 = 0
+            print("Normal")
+        case "gray":
+            item2 = 100
+            print("Extra 100 rs")
         case "pink":
-            item2 = "pink"
-            print("extra 50 rs")
-    match c:
+            item2 = 50
+            print("Extra 50 rs")
         case "blue":
-            item2 = "blue"
-            print("extra 20 rs")  
-    return item = item2  
+            item2 = 20
+            print("Extra 20 rs")
+
+    return item2
+
+# Check if the script is being run as the main program
+if __name__ == "__main__":
+    main()  # Call the main function
